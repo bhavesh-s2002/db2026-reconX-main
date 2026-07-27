d0682cb41cca487ec6f1a064961ea6609ff140c2
Rohith <rohithbiradar2004@gmail.com>
2026-07-27 17:23:32 +0530

    Day 1 resolve

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-27T03:33:07-0700",
-    comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
+    date = "2026-07-27T17:00:01+0530",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.12 (Oracle Corporation)"
 )
 @Component
 public class TradeMapperImpl implements TradeMapper {
